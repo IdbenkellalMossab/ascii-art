4c75b9c9c4e9119684add9f93c89123260e8f460
Mossab Idbenkellal <idmossab@gmail.com>
2024-10-10 14:51:40 +0100

    ascii-art-fs

--- main.go
+++ main.go
@@ -9,23 +9,44 @@ import (
 	ascii_art "ascii_art/Functions"
 )
 
-
-
 func main() {
-
-	arg:=os.Args[1:]
+	//==================================================== asci-art1 v1 ==================================
+	/*arg:=os.Args[1:]
 	// check if numbers args not equal 1
 	if len(arg) != 1 {
 		fmt.Println("the numbers of args not correct")
 		return
 	}
 
-	// open file 
+	// open file
 	file, err := os.Open("./Symbols/standard.txt")
 	if err != nil {
 		fmt.Println("Erreur:", err)
 		return
+	}*/
+	//==================================================== asci-art1-fs  ==================================================
+	banner := "standard"
+	// Check if the user passed exactly one argument.
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
+		return
 	}
+
+	if len(os.Args) == 3 {
+		if os.Args[2] != "standard" && os.Args[2] != "shadow" && os.Args[2] != "thinkertoy" {
+			fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
+		} else {
+			banner = os.Args[2]
+		}
+	}
+
+	// Open the file containing the symbol definitions.
+	file, err := os.Open("./Symbols/" + banner + ".txt")
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
 	defer file.Close() // Ensures the file is closed after use.
 
 	sentence := os.Args[1]
@@ -52,7 +73,7 @@ func main() {
 			symbole = []string{}
 			count = 0
 		}
-		
+
 	}
 	// Checks if the file contains at least 95 symbols.
 	if len(symboles) < ascii_art.Nbr_char_printble {
